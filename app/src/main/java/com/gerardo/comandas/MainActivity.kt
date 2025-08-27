b28deeb5cadf08c0f400e837aca8a6666f2a75f3
GerardoPerezArenas <gerardo.perez@bizkaia.eu>
2025-08-27 15:17:29 +0200

    feat: add animated navigation graph

--- app/src/main/java/com/gerardo/comandas/MainActivity.kt
+++ app/src/main/java/com/gerardo/comandas/MainActivity.kt
@@ -23,9 +23,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+import com.gerardo.comandas.navigation.NavGraph
 import com.gerardo.comandas.ui.theme.COMANDASTheme
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.TextField
@@ -47,49 +46,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             COMANDASTheme {
-                val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = "main_screen") {
-                    composable("main_screen") { MainScreen(navController = navController) }
-                    composable("login_screen") { LoginScreen(navController = navController) }
-                    composable("zonas_screen") { ZonasScreen(navController = navController) }
-                    composable("barra_screen") { BarraScreen(navController = navController) }
-                    composable("barra_comer_aqui") { BarraComerAquiScreen(navController = navController) }
-                    composable("barra_para_llevar") { BarraParaLlevarScreen(navController = navController) }
-                    composable("comedor_bar") { ComedorBarScreen(navController = navController) }
-                    composable("comedor_principal") { ComedorPrincipalScreen(navController = navController) } // Agregada la pantalla ComedorPrincipalScreen
-                    composable("barra_comer_aqui_mesa_{mesaId}") { backStackEntry ->
-                        val mesaId = backStackEntry.arguments?.getString("mesaId")?.toIntOrNull()
-                        if (mesaId != null) {
-                            BarraComerAquiMesaScreen(navController, mesaId)
-                        }
-                    }
-                    composable("comedor_bar_mesa_{mesaId}") { backStackEntry ->
-                        val mesaId = backStackEntry.arguments?.getString("mesaId")?.toIntOrNull()
-                        if (mesaId != null) {
-                            ComedorBarMesaScreen(navController, mesaId)
-                        }
-                    }
-                    composable("comida_screen") { ComidaScreen(navController) }
-                    composable("ingredientes_screen/{nombre}/{ingredientes}") { backStackEntry ->
-                        val nombre = backStackEntry.arguments?.getString("nombre") ?: ""
-                        val ingredientes = backStackEntry.arguments?.getString("ingredientes") ?: ""
-                        IngredientesScreen(navController, nombre, ingredientes)
-                    }
-                    composable("bebida_screen") {
-                        BebidaScreen(navController, showButtons = false)
-                    }
-                    composable("detalle_bebida_screen/{nombre}/{descripcion}") { backStackEntry ->
-                        val nombre = backStackEntry.arguments?.getString("nombre") ?: ""
-                        val descripcion = backStackEntry.arguments?.getString("descripcion") ?: ""
-                        DetalleBebidaScreen(navController, nombre, descripcion)
-                    }
-                    composable("comedor_principal_mesa_{mesaId}") { backStackEntry ->
-                        val mesaId = backStackEntry.arguments?.getString("mesaId")?.toIntOrNull()
-                        if (mesaId != null) {
-                            ComedorPrincipalMesaScreen(navController, mesaId)
-                        }
-                    }
-                }
+                val navController = rememberAnimatedNavController()
+                NavGraph(navController = navController)
             }
         }
     }
